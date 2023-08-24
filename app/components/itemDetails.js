8e188f9cfbce7aed4c7dd4a612e59e43b1575962
The-TigerBeast <89836536+The-TigerBeast@users.noreply.github.com>
2023-08-24 16:32:11 +0530

    Added some more items

--- app/components/itemDetails.js
+++ app/components/itemDetails.js
@@ -22,6 +22,38 @@ const itemDetails = [
         price: 99,
         quantity: 'Quantity',
         totalPrice: 'Total price'
+    },
+    {
+        id:4,
+        name: 'Veg Thali',
+        img: 'https://imgk.timesnownews.com/story/veg-thali.gif',
+        price: 150,
+        quantity: 'Quantity',
+        totalPrice: 'Total price'
+    },
+    {
+        id:5,
+        name: 'Non-Veg Thali',
+        img: 'https://im.hunt.in/cg/Darjeeling/City-Guide/Non-Veg-Restaurants-Darjeeling2.png',
+        price: 200,
+        quantity: 'Quantity',
+        totalPrice: 'Total price'
+    },
+    {
+        id:6,
+        name: 'Chicken Butter Masala',
+        img: 'https://www.indianhealthyrecipes.com/wp-content/uploads/2022/07/chicken-butter-masala-recipe.jpg',
+        price: 150,
+        quantity: 'Quantity',
+        totalPrice: 'Total price'
+    },
+    {
+        id:7,
+        name: 'Paneer Butter Masala',
+        img: 'https://www.vegrecipesofindia.com/wp-content/uploads/2020/01/paneer-butter-masala-5.jpg',
+        price: 130,
+        quantity: 'Quantity',
+        totalPrice: 'Total price'
     }
   ]
   export default itemDetails
